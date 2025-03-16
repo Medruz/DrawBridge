c10c298086259992f4db90fbd864bbe66ca487f8
Medruz <luca.medri.stud@ispascalcomandini.it>
2025-03-16 17:05:40 +0100

    Tentativo di metodo passaggio()

--- Models/GestioneTraffico.cs
+++ Models/GestioneTraffico.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DrawbridgeSimulator.Models
@@ -15,13 +16,14 @@ namespace DrawbridgeSimulator.Models
         private const int NrAutoSulPonte = 3;
         private SemaphoreSlim sSlim;
         private readonly int width = Console.WindowWidth;
-        private Task[] tasks = new Task[2];
+        private List<Task> tasks;
         public GestioneTraffico()
         {
             VeicoliDx = new List<Veicolo>();
             VeicoliSx = new List<Veicolo>();
             Bridge = new Ponte();
             sSlim = new SemaphoreSlim(NrAutoSulPonte);
+            tasks = new List<Task>();
         }
 
         
@@ -30,7 +32,7 @@ namespace DrawbridgeSimulator.Models
             //Thread tDx = new Thread(AttraversaDxSx);
             ConsoleKeyInfo lettera;
             bool fine = false;
-            
+
             //int height = Console.WindowHeight;
             do
             {
@@ -52,7 +54,9 @@ namespace DrawbridgeSimulator.Models
                 for (int i = 0; i < VeicoliSx?.Count; i++)
                 {
                     Console.SetCursorPosition(5,i+4);
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine(VeicoliSx[i].ToString());
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
 
                 // Scrivo le auto a dx
@@ -86,7 +90,8 @@ namespace DrawbridgeSimulator.Models
 
                     // Avvia il movimento delle macchine
                     case ConsoleKey.P:
-                        AttraversaDxSx();
+                        Passaggio();
+                        Console.ForegroundColor = ConsoleColor.White;
                         break;
 
                     // Killa il programma
@@ -101,67 +106,118 @@ namespace DrawbridgeSimulator.Models
             } while (fine == false);  
         }
 
-        public void Passaggio()
+        public async void Passaggio()
         {
-            List<string> autos = new List<string>();
-            int count = 0;
+            //<string> autos = new List<string>();
+            //int count = 0;
+            int pos;
+
+            //do
+            //{
+            //    autos.Add(VeicoliDx[count].ToString());
+            //    count++;
+            //} while (autos.Count < NrAutoSulPonte && autos.Count != VeicoliDx.Count);
+
+
+            //creo una lista di task per ogni macchina a cui affido il metodo di attraversamento
+            //con il semaforeslim controllo quando i task agiscono
+            //quando clicco il tasto P tutti i veicoli aggiunti devono attraversare il ponte, prima dx poi sx
             do
             {
-                autos.Add(VeicoliDx[count].ToString());
-                count++;
-            } while (autos.Count < NrAutoSulPonte || autos.Count != VeicoliDx.Count);
-            for(int i = 0; i < 32; i++)
-            {
-
-            }
+                if (VeicoliDx?.Count != 0)
+                {
+                    pos = 27;
+                    for (int i = 0; i < NrAutoSulPonte; i++)
+                    {
+                        tasks.Add(AttraversaDxSx(i, pos));
+                    }
+                    for (int i = 0; i < 9; i++)
+                    {
+                        for(int k = 0; k < tasks.Count; k++)
+                        {
+                            await sSlim.WaitAsync();
+                            try
+                            {
+                                // Esegue il task asincrono
+                                await AttraversaDxSx(k, pos);
+                            }
+                            finally
+                            {
+                                // Rilascia il semaforo per consentire ad altri task di eseguire
+                                sSlim.Release();
+                            }
+                        }
+                        
+                        pos += 5;
+                        Thread.Sleep(200);
+                    }
+                    VeicoliDx.Remove(VeicoliDx[0]);
+                    VeicoliDx.Remove(VeicoliDx[0]);
+                    VeicoliDx.Remove(VeicoliDx[0]);
+                    for(int j = 0; j < NrAutoSulPonte; j++)
+                    {
+                        Console.SetCursorPosition(width - 72, j+13);
+                        Console.WriteLine("              ");
+                    }
+                }
+                if (VeicoliSx?.Count != 0)
+                {
+                    pos = 20;
+                    for (int i = 0; i < 9; i++)
+                    {
+                        AttraversaSxDx(pos);
+                        pos += 5;
+                        Thread.Sleep(200);
+                    }
+                }
+            } while (VeicoliDx.Count != 0 || VeicoliSx.Count != 0);
         }
 
-        public async Task AttraversaDxSx()
+        public async Task AttraversaDxSx(int i, int pos)
         {
             //Prima parte in un metodo secondario(passaggio) dove poi chiamo per il numero di auto il task con questo metodo
             // SetCursorPosition per rappresentare l'attraversamento
-            if (VeicoliDx != null)
-            {
-                List<string> autos = new List<string>();
-                int count = 0; 
-                do
-                {
-                    autos.Add(VeicoliDx[count].ToString());
-                    count++;
-                } while (autos.Count < NrAutoSulPonte || autos.Count != VeicoliDx.Count);
-                int pos = 27;
-                //seconda parte
-                for (int j = 0; j < 32; j++)
-                {
-                    //questo primo for non ci va
-                    for (int i = 0; i < NrAutoSulPonte; i++)
-                    {
-                        if (j == 0)
-                        {
-                            //da mettere nel metodo passaggio
-                            Console.SetCursorPosition(width - pos, i + 13);
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(VeicoliDx?[i].ToString());
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.SetCursorPosition(width - pos - 5, i + 13);
-                            Console.WriteLine(" ");
-                            Console.SetCursorPosition(width - pos, i + 13);
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(VeicoliDx?[i].ToString());
-                        }
-                        else
-                        {
-                           
-                            Console.SetCursorPosition(width - pos, i + 13);
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(VeicoliDx?[i].ToString());
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
+            
+            //List<string> autos = new List<string>();
+            //int count = 0; 
+            //do
+            //{
+            //    autos.Add(VeicoliDx[count].ToString());
+            //    count++;
+            //} while (autos.Count < NrAutoSulPonte || autos.Count != VeicoliDx.Count);
+            //int pos = 27;
+            //seconda parte
+            //questo primo for non ci va
+            //for (int i = 0; i < NrAutoSulPonte; i++)
+            //{
 
-                    }
-                    pos+=5;
-                }
+                //da mettere nel metodo passaggio
+                Console.SetCursorPosition(width - (pos - 5), i + 13);
+                Console.WriteLine("          ");
+                Console.SetCursorPosition(width - pos, i + 13);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(VeicoliDx?[i].ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+                //Console.SetCursorPosition(width - pos, i + 13);
+                //Console.ForegroundColor = ConsoleColor.Green;
+                //Console.WriteLine(VeicoliDx?[i].ToString());
+                //Console.ForegroundColor = ConsoleColor.White;
+            //}
+            //await Task.CompletedTask;
+        }
+
+        public async Task AttraversaSxDx(int pos)
+        {
+            for (int i = 0; i < NrAutoSulPonte; i++)
+            {
+                Console.SetCursorPosition(width - (pos - 5), i + 13);
+                Console.WriteLine("          ");
+                Console.SetCursorPosition(width - pos, i + 13);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(VeicoliDx?[i].ToString());
+                Console.ForegroundColor = ConsoleColor.White;
             }
+            await Task.CompletedTask;
         }
 
         public void Acqua(int startRow)
